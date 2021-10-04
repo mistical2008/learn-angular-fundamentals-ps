a903829f7dcd5b58a68462cd553924bfc80884c2
Evgeniy Talko <etalko@gmail.com>
2021-10-04 11:25:25 +0300

    Add: ngSwitch, ngClass

--- src/app/event/event.component.ts
+++ src/app/event/event.component.ts
@@ -7,17 +7,24 @@ import { EventEmitter, Component, Input, OnInit, Output } from '@angular/core';
       <div class="well hoverwell thumbnail">
         <h2>{{event?.name}}</h2>
         <div>Date: {{event?.date}}</div>
-        <div>Time: {{event?.time}}</div>
+        <div [ngSwitch]="event?.time">
+          Time: {{event?.time}}
+          <span [ngClass]="getEarliyStartClasses()" *ngSwitchCase="'8:00 am'">(Early start)</span>
+          <span *ngSwitchCase="'9:00 am'">(Normal start)</span>
+          <span *ngSwitchDefault="'10:00 am'">(Late start)</span>
+        </div>
         <div>Price: \${{ event?.price }}</div>
-        <div>
+        <div [hidden]="!event?.location">
           <span>Location: {{event?.location?.address}}</span>
           <span class="pad-left">{{event?.location?.city}}, {{event?.location?.country}}</span>
         </div>
-        <div *ngIf="event.onlineUrl">Online url: {{event?.onlineUrl}}</div>
+        <div *ngIf="event?.onlineUrl">Online url: {{event?.onlineUrl}}</div>
       </div>
     </div>
   `,
   styles: [`
+      .green { color: #009900;}
+      .bold { font-weight: 800; }
       .thumbnail { min-height: 210px;}
       .pad-left {margin-left: 10px;}
       .well {color: #bbb;}
@@ -29,6 +36,12 @@ export class EventComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+  getEarliyStartClasses() {
+    if (this.event?.time === '8:00 am') {
+      return ['green', 'bold'];
+    }
+    return [];
   }
 
 }
