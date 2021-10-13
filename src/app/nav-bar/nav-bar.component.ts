12cae80a892d8d95fd14d37d7e716d83da90d49e
Evgeniy Talko <etalko@gmail.com>
2021-10-13 21:25:55 +0300

    Add: active link style application

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,19 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-nav-bar',
-  templateUrl: './nav-bar.component.html',
-  styles: [`
-    .nav.navbar-nav { font-size: 15px;}
-    #searchForm { margin-right: 100px;}
-    @media (max-width: 1200px) { #searchForm {display: none;}}
-    `]
+  selector: "app-nav-bar",
+  templateUrl: "./nav-bar.component.html",
+  styles: [
+    `
+      .nav.navbar-nav {
+        font-size: 15px;
+      }
+      li > a.link--active {
+        color: tomato;
+      }
+      #searchForm {
+        margin-right: 100px;
+      }
+      @media (max-width: 1200px) {
+        #searchForm {
+          display: none;
+        }
+      }
+    `,
+  ],
 })
 export class NavBarComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
