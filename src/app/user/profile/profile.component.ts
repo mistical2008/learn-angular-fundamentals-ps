e0613e3008f32b34b6bb5478bda77829fe4569db
Evgeniy Talko <etalko@gmail.com>
2021-11-04 22:09:44 +0300

    Add(profile): form validation

--- src/app/user/profile/profile.component.ts
+++ src/app/user/profile/profile.component.ts
@@ -1,23 +1,67 @@
 import { Component, OnInit } from "@angular/core";
-import { FormControl, FormGroup } from "@angular/forms";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
 import { Router } from "@angular/router";
 import { AuthService } from "../login-form/auth.service";
 
 @Component({
   selector: "user-profile",
   templateUrl: "./profile.component.html",
-  styles: [],
+  styles: [
+    `
+      .error em {
+        color: #e05c65;
+        float: right;
+        padding-left: 10px;
+      }
+      .error input {
+        background-color: #e05c65;
+      }
+      .error :placeholer {
+        color: #ffffff
+      }
+      .error ::webkit-input-placeholder {
+        color: #ffffff;
+      }
+      .error ::moz-placeholder {
+        color: #ffffff;
+      }
+      .error :moz-placeholder {
+        color: #ffffff;
+      }
+      .error :ms-input-placeholder {
+        color: #ffffff;
+      }
+    `,
+  ],
 })
 export class ProfileComponent implements OnInit {
   profileForm: FormGroup;
+  private firstName: FormControl;
+  private lastName: FormControl;
 
   constructor(private auth: AuthService, private router: Router) {}
 
   ngOnInit(): void {
-    this.profileForm = new FormGroup({
-      firstName: new FormControl(this.auth.currentUser.firstName),
-      lastName: new FormControl(this.auth.currentUser.lastName),
-    });
+    (this.firstName = new FormControl(
+      this.auth.currentUser.firstName,
+      [Validators.required, Validators.pattern('[a-zA-Z].*')]
+    )),
+      (this.lastName = new FormControl(
+        this.auth.currentUser.lastName,
+        Validators.required
+      )),
+      (this.profileForm = new FormGroup({
+        firstName: this.firstName,
+        lastName: this.lastName,
+      }));
+  }
+
+  isValidFirstName() {
+    return this.firstName.valid || this.firstName.untouched;
+  }
+
+  isValidLastName() {
+    return this.lastName.valid || this.firstName.untouched;
   }
 
   saveProfile(values: { firstName: string; lastName: string }) {
